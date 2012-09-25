881dcf1561eee00e5718f176dcddcc26efd6470d
pipe2grep <manny.a.huerta@gmail.com>
2012-09-24 21:17:36 -0700

    added search by location or geocoordinates.. provide a collection of either or and return all relevant info as a structured mobmonkey location list response

--- src/com/MobMonkey/Resources/SearchResource.java
+++ src/com/MobMonkey/Resources/SearchResource.java
@@ -1,5 +1,6 @@
 package com.MobMonkey.Resources;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -28,17 +29,55 @@ public class SearchResource extends ResourceHelper {
 	public Response findLocationsInJSON(Location loc) {
 
 		if (loc.getLatitude() != null && loc.getLongitude() != null) {
-			//TODO validate lat/long with regex
+			// TODO validate lat/long with regex
 			List<Location> locations = SearchHelper.getLocationsByGeo(loc);
 			return Response.ok().entity(locations).build();
 		}
-		if(loc.getLocality() != null && loc.getRegion() != null && loc.getPostcode() != null){
-			//TODO validate postcode with regex, make sure locality and region are sane
+		if (loc.getLocality() != null && loc.getRegion() != null
+				&& loc.getPostcode() != null) {
+			// TODO validate postcode with regex, make sure locality and region
+			// are sane
 			List<Location> locations = SearchHelper.getLocationsByAddress(loc);
 			return Response.ok().entity(locations).build();
-			
+
 		}
-		return Response.status(500).entity(new Status("Failure", "You need to specify either (lat & long) OR (address, locality, region & zip)", "")).build();
+		return Response
+				.status(500)
+				.entity(new Status(
+						"Failure",
+						"You need to specify either (lat & long) OR (address, locality, region & zip)",
+						"")).build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/glob")
+	public Response findLotsOfLocationsInJSON(List<Location> locs) {
+
+		List<Location> locations = new ArrayList<Location>();
+		for (Location loc : locs) {
+			if (loc.getLatitude() != null && loc.getLongitude() != null) {
+				// TODO validate lat/long with regex
+				List<Location> locList = SearchHelper.getLocationsByGeo(loc);
+				locations.addAll(locList);
+				
+			}
+			if (loc.getLocality() != null && loc.getRegion() != null
+					&& loc.getPostcode() != null) {
+				// TODO validate postcode with regex, make sure locality and
+				// region are sane
+				List<Location> locList = SearchHelper
+						.getLocationsByAddress(loc);
+				locations.addAll(locList);
+
+			}
+		}
+		
+		
+		return Response
+				.ok()
+				.entity(locations).build();
 	}
 
 	@GET
