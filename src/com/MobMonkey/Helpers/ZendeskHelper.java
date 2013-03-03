8ff76f6c9fdcfcb83b5c4797884c81c138f0da41
Manny Huerta <mannyahuerta@gmail.com>
2013-03-03 12:26:30 -0700

    created method to create ticket @ zendesk

---
+++ src/com/MobMonkey/Helpers/ZendeskHelper.java
@@ -0,0 +1,35 @@
+package com.MobMonkey.Helpers;
+
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+
+public final class ZendeskHelper {
+
+	static final String ZENDESK_USERNAME = "rashaad@tappforce.com";
+	static final String ZENDESK_PASSWORD = "taptap85";
+	static final String ZENDESK_RESOURCE_URI = "https://mobmonkey.zendesk.com";
+
+	public ZendeskHelper() {
+
+	}
+
+	public bool createTicket() {
+		Client client = Client.create();
+		WebResource webResource = client.resource(ZENDESK_RESOURCE_URI
+				+ "/api/v2/tickets.json");
+
+		client.addFilter(new HTTPBasicAuthFilter(ZENDESK_USERNAME,
+				ZENDESK_PASSWORD));
+		
+		String input = "{\"ticket\":{\"subject\":\"Inappropriate Content\",\"comment\":{\"body\":\"The media uploaded has been deemed inappropriate by the requestor. Please review here.\"}}}";
+
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class, input);
+
+	}
+}
